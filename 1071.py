a4850c87c038d47911f7b691dca5a31f240accb6
Thiago A. de Souza <thiago.souza27@hotmail.com>
2023-03-09 07:53:58 -0300

    1071 - final

--- 1071.py
+++ 1071.py
@@ -1,1 +1,13 @@
-x,y = int(input())
+x = int(input())
+y = int(input())
+inicio = min(x,y)+1
+fim = max(x,y)
+
+if inicio %2 == 0:
+    inicio += 1
+
+i = 0 
+for loop in range(inicio,fim,2):
+    i += loop
+
+print(i)
